b38f1a37a8ed454e328ecb292e432014cae37a45
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2024-02-29 13:48:02 -0300

    calculo_imc.cpp

--- calculo_imc.cpp
+++ calculo_imc.cpp
@@ -2,47 +2,54 @@
 
 using namespace std;
 
-float calcular_imc(float massa, float altura){
-    return massa / (altura*altura);
+//função que recebe o par (massa, altura) e retorna o IMC
+float calcular_imc(float fmassa, float faltura){
+    return fmassa / (faltura*faltura);    //cálculo do IMC
 }
 
-void analisar_imc(float resultado){
-    if (resultado < 17){
-        cout << "Você está muito abaixo do peso!" << endl;
+//função que recebe o valor do IMC, mas não retorna nada, por isso usamos o void.
+//o objetivo dessa função é caracterizar as faixas de IMC e posteriormente informar no console em qual faixa de IMC a pessoa se encontra.
+void analisar_imc(float fresultado){
+    if (fresultado < 17){
+        cout << "Voce esta muito abaixo do peso!" << endl;
     }
-    else if(resultado < 18.5){
-        cout << "Você está abaixo do peso!" << endl;
+    else if(fresultado < 18.5){
+        cout << "Voce esta abaixo do peso!" << endl;
     }
-    else if(resultado < 25){
-        cout << "Seu peso é normal!" << endl;
+    else if(fresultado < 25){
+        cout << "Seu peso esta normal!" << endl;
     }
-    else if(resultado < 30){
-        cout << "Você está acima do peso!" << endl;
+    else if(fresultado < 30){
+        cout << "Voce esta acima do peso!" << endl;
     }
-    else if(resultado < 35){
-        cout << "Você está com obsesidade!" << endl;
+    else if(fresultado < 35){
+        cout << "Voce esta com obsesidade!" << endl;
     }
-    else if(35 <= resultado < 40){
-        cout << "Você está com obesidade severa!" << endl;
+    else if(35 <= fresultado < 40){
+        cout << "Voce esta com obesidade severa!" << endl;
     }
     else {
-        cout << "Você está com obesidade mórbida!" << endl;
+        cout << "Voce esta com obesidade morbida!" << endl;
     }
 }
 
+//definindo a função principal
 int main() {
     float fmassa, faltura;
 
-    cout << "Qual é a sua massa? ";
+    //usuário informa sua massa no console
+    cout << "Digite sua massa em kg:  ";
     cin >> fmassa; 
 
-    cout << "Qual é a sua altura? ";
+    //usuário informa sua altura no console
+    cout << "Digite sua altura em metros: ";
     cin >> faltura;
 
-    float resultado = calcular_imc(fmassa, faltura);
+    //usando uma função definida anteriormente para calcular o IMC dado os valores fornecidos pelo usuário
+    float fresultado = calcular_imc(fmassa, faltura);
 
-    analisar_imc(resultado);
+    //definindo a faixa de imc do usuário
+    analisar_imc(fresultado);
 
-
-    return 0; 
+    return 0;
 }
